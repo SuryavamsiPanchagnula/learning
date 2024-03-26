09fff48c363ceb09483eacc03a5d1e6dd36bc9eb
Admin <psuryavamsi88@gmail.com>
2024-03-27 00:42:08 +0530

    first commit

---
+++ firstone/src/firstone/firstone.java
@@ -0,0 +1,10 @@
+package firstone;
+
+public class firstone {
+	
+	public static void main(String[] args) {
+		
+		System.out.print("Hello world");
+	}
+
+}
---
+++ firstone/src/firstone/second.java
@@ -0,0 +1,16 @@
+package firstone;
+
+public class second {
+	
+	public static void main(String[] args) {
+		
+		int a,b,c;
+		
+		a=10;
+		b=19;
+		c=a+b;
+		
+		System.out.print("Addition "+c);
+	}
+
+}
